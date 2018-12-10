895572a0b5af41b6ed59d2bf67df8b83f7f263b2
margreta <margreta11@ru.is>
2018-12-10 23:47:04 +0000

    somehow the main didn't go through (main was the one we lost completely)

--- Iceberg_classes/main.py
+++ Iceberg_classes/main.py
@@ -3,6 +3,15 @@ from ui.dealer_ui import Dealer_Ui
 from ui.admin_ui import Admin_Ui
 from models.car import Car
 
+def car_type_price(car_type):
+    if car_type == "A":
+        price = 4000
+    elif car_type == "B":
+        price = 3000
+    elif car_type == "C":
+        price = 2000 
+    return price 
+
 
 def main():
     ui = Home_page()
@@ -15,21 +24,77 @@ def main():
     if username == "dealer":
         choice = dealer_ui.home_page()
         if choice == 1: 
+            #initializing variables: 
+            repeat = "y"
             confirm = "n"
-            while confirm == "n":
-                name, driver_license, email, phone_num = dealer_ui.create_booking_1_of_5()
-                confirm = dealer_ui.confirm_customer(name)
-                contin = dealer_ui.options()
-                if contin == "1":
-                    card_num, valid, cvc = dealer_ui.create_booking_2_of_5()
-                elif contin == "2":
-                    continue
-                elif contin == "3":
-                    dealer_ui.home_page()
+            skip_option = "n"
 
+            while repeat == "y":
+                while confirm == "n":
+                    name, driver_license, email, phone_num = dealer_ui.create_booking_1_of_5()
+                    confirm = dealer_ui.confirm_customer(name)
+                    current_page = 1
+                
+                if skip_option == "n":
+                    contin = dealer_ui.options()
+            
+                if contin == "1":
+                    if current_page == 1:
+                        card_num, valid, cvc = dealer_ui.create_booking_2_of_5(name)
+                        current_page += 1
+
+                    elif current_page == 2:
+                        start_date, amount_of_days, car_type = dealer_ui.create_booking_3_of_5()
+                        price = car_type_price(car_type)
+                        current_page += 1
+
+                    elif current_page == 3: 
+                        #initiate the price counter: 
+                        total_price = 0 
+                        skip_option = "n"
+
+                        dealer_ui.create_booking_4_of_5()
+                        more_extras = "y"
+                        while more_extras == "y":
+                            kasko_or_child_seat, more_extras = dealer_ui.extras()
+                            if kasko_or_child_seat == "1": 
+                                total_price += 1
+                            elif kasko_or_child_seat == "2":
+                                total_price += 50
+                        current_page += 1
+
+                    elif current_page == 4:
+                        #Get total amount to charge:
+                        total_amount = price + total_price
+                        #How is customer paying:
+                        billing_type = dealer_ui.create_booking_5_of_5()
+                        
+                        if billing_type == "1":
+                            card_name, card_number, validation_time = dealer_ui.credit_debit_card()
+                        elif billing_type == "2":
+                            comp_name, amount, due_date = dealer_ui.billing_to()
+                        elif billing_type == "3":
+                            paid_amount = int(dealer_ui.cash(total_amount))
+                            change = total_amount - paid_amount
+                            dealer_ui.print_change(change)
+                        elif billing_type == "4":
+                            current_page -= 1
+                            skip_option = "y"
+                        elif billing_type == "5":
+                            confirm = "n"
+                            continue
+                        if billing_type in range(1,4):
+                            again = dealer_ui.confirm_billing()
+                            if again == "n":
+                                #Start over:
+                                continue
+                    
+                elif contin == "2" or contin == "3":
+                    confirm = "n"
+                    continue
 
         # if choice == 2:
-            
+            #Næsta aðgerð fyrir dealer (change booking) 
 
     
     elif username == "admin":
@@ -40,21 +105,4 @@ def main():
             admin_ui.remove_car()
         elif choice == 3:
             admin_ui.mark_repair()
-        elif choice == 4:
-            Home_page.home
-        
-
-
-
-
-
-
-
-    
-
-
-
-    
-
-
-main()
+main()        
